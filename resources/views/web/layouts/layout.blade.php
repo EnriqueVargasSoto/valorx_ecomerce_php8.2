312ee688bc90a3b1e2fc3c6cbcb1eee0beada083
EnriqueVargasSoto <kikevargas23@gmail.com>
2023-10-25 21:36:32 -0500

    25/10/2023 19.36

--- resources/views/web/layouts/layout.blade.php
+++ resources/views/web/layouts/layout.blade.php
@@ -2,7 +2,8 @@
 <html lang="es">
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0, viewport-fit=cover">
+
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="icon" href="{{ asset('web/assets/images/favicon/1.png')}}" type="image/x-icon">
     <title>Atiendo</title>
